886980497f52d3b879cf322b4af97e6c748d384d
sandeep-sparrow <sandeep.p4856@gmail.com>
2021-09-01 15:29:25 +0530

    Added GET: POST: PUT: DELETE:

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using CmdApi.DAL;
+using CmdApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,8 +20,63 @@ namespace CmdApi.Controllers{
 
         //GET:      api/commands
         [HttpGet]
-        public ActionResult<IEnumerable<string>> GetString(){
-            return new string[] {"This", "Is", "New", "Mainframe"};
+        public ActionResult<IEnumerable<Command>> GetCommands(){
+
+            return _context.CommandItems;
+        }
+
+        //GET:      api/commands/id
+        [HttpGet("{Id}")]
+        public ActionResult<Command> GetCommandItem(int id){
+
+            var commandItem = _context.CommandItems.Find(id);
+            if(commandItem == null){
+                return NotFound();
+            }
+
+            return commandItem;
+
+        }
+
+        //POST:     api/commands
+        [HttpPost]
+        public ActionResult<Command> PostCommandItem(Command command){
+
+            _context.CommandItems.Add(command);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetCommandItem", new Command{Id = command.Id}, command);
+  
+        }
+
+        //PUT:      api/command
+        [HttpPut("{Id}")]
+        public ActionResult PutCommandItem(int id, Command command){
+
+            if(id != command.Id){
+                return BadRequest();
+            }
+
+            _context.Entry(command).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        //DELETE:       api/commands/id
+        [HttpDelete("{Id}")]
+        public ActionResult<Command> DeleteCommandItem(int id){
+
+            var commandItem = _context.CommandItems.Find(id);
+
+            if(commandItem == null){
+                return NotFound();
+            }
+
+            _context.CommandItems.Remove(commandItem);
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
